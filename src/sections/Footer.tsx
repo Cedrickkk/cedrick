22645be62b2eca212267285ca6cce22da7f374f0
John Cedric Panti <inhog.cedrick@gmail.com>
2025-01-19 09:48:19 +0800

    fixed: footer

--- src/sections/Footer.tsx
+++ src/sections/Footer.tsx
@@ -1,6 +1,6 @@
 export default function Footer() {
   return (
-    <footer className="container mx-auto my-4 flex max-h-screen w-full justify-center text-center text-xs text-muted-foreground">
+    <footer className="container mx-auto my-4 flex w-full justify-center text-center text-xs text-muted-foreground">
       &copy; 2024 Designed and built by John Cedric Panti | All rights reserved.
     </footer>
   );

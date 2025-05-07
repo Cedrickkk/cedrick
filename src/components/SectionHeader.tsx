d3dc4d5c30f13b04f442d9700b79d8a9adfd9f7f
John Cedric Panti <inhog.cedrick@gmail.com>
2025-05-07 11:24:13 +0800

    Update headers textsize

--- src/components/SectionHeader.tsx
+++ src/components/SectionHeader.tsx
@@ -5,7 +5,7 @@ type SectionHeaderProps = {
 export default function SectionHeader({ children }: SectionHeaderProps) {
   return (
     <div className="mb-5 flex items-center gap-3">
-      <h2 className="text-xl font-semibold uppercase tracking-wide">
+      <h2 className="text-sm font-semibold uppercase tracking-wide lg:text-xl">
         {children}
       </h2>
       {/* <span className="block h-[.5px] w-full max-w-28 bg-primary"></span> */}
